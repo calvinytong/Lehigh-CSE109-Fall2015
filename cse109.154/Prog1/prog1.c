9584c7d734dc07ab5420e72884c5c49021830625
Calvin Tong <cyt219@miranda.cse.lehigh.edu>
2015-09-01 13:04:20 -0400

    working uppercase

--- cse109.154/Prog1/prog1.c
+++ cse109.154/Prog1/prog1.c
@@ -7,8 +7,45 @@
  * */
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char[] *argv)
+int write(int upper, int reverse)
 {
- 
+ char c;
+ char buf[256];
+ while((c = getchar()) != EOF)
+ {
+  if(upper)
+  {
+   c = toupper(c);
+  }
+  putchar(c);
+ }
+ return 0; 
+}
+
+int main(int argc, char *argv[])
+{
+ int upper = 0;
+ int reverse = 0;
+ int i;
+
+ for(i = 1; i < argc; i++)
+ { 
+  if(strcmp(argv[i], "-u") == 0)
+  {
+   upper = 1;
+   continue;
+  }
+  
+  if(strcmp(argv[i], "-r") == 0)
+  {
+   reverse = 1;
+   continue;
+  }  
+  //perror("Invalid command line option");
+ }
+ write(upper, reverse);
+
+ return 0; 
 }
