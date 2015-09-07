c2c67a7ec838e133049670466fa4a2d3b14b5c65
Calvin Tong <cyt219@dactyl.cse.lehigh.edu>
2015-09-07 15:29:54 -0400

    prog2 in working condition and commented

--- cse109.154/Prog2/getopts.c
+++ cse109.154/Prog2/getopts.c
@@ -1,47 +1,75 @@
+/*
+ * CSE 109
+ * Calvin Tong
+ * cyt219
+ * implements the getopts prototype defined in prog2.c, parses the args the user enters
+ * Program #2
+ */
+
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-char **strarray;
-
+/*
+ * parses the users command line argument and returns them in a string array
+ * @param command that the user types in stdin
+ * @return char** the string array holding the tokenized cmdline args with a 0 as the last element
+ */
 char **getopts(char cmd[])
 {
- char *token;
- int tokcount;
-
+ //the string array that will hold the parsed command line args
+ char **strarray;
+ 
+ //copy the array into a new string array so we can use strtok to count the args without editing 
+ //the original string
  int len = strlen(cmd);
  char copy[len + 1];
- strcpy(cmd, copy);
+ strcpy(copy, cmd);
+ 
+ //the number of tokens that have been read
+ int tokcount = 0;
 
- token = strtok(copy, " ");
- tokcount = 1;
+ //count the number of tokens in the inputed array
+ //gets the first token in the string
+ char *token = strtok(copy, " ");
  while(token != NULL)
  {
-  token = strtok(NULL, cmd);
+  //increment the token counter
   tokcount++;
+  //grab the next token
+  token = strtok(NULL, " ");
  }
-
+ 
+ //use the number of tokens found to dynamically allocate the string array
+ //adding 1 because we need to put a 0 onto the end of the array
  strarray = malloc((tokcount + 1) * sizeof(char*));
  
- printf("first malloc \n"); 
- 
- int i = 0;
+ //reset the tok count to 0 for the second iteration
+ //on this iteration it is used as an index rather than just a count
+ tokcount = 0;
 
+ //since the copied array has been modified during the token count use the cmd array
+ //for the second iteration
+ 
+ //grab the first token
  token = strtok(cmd, " ");
- printf("token \n");
- strarray[i] = malloc((strlen(token) + 1) * sizeof(char));
- printf("second malloc\n");
- strcpy(strarray[i], token);
- printf("first token");
  while(token != NULL)
  {
-  token = strtok(NULL, cmd);
-  strarray[i] = malloc((strlen(token) + 1) * sizeof(char));
-  strcpy(strarray[i], token);
-  i++;
-  printf("sup i'm a while loop \n");
- }
+  //use the length of the token to dynamically allocate elements of strarray
+  //adding one to acomidate the null termination
+  strarray[tokcount] = malloc((strlen(token) + 1) * sizeof(char));
 
- strarray[i] = "0";
+  //copy the token into the correct index of the string array
+  strcpy(strarray[tokcount], token);
+
+  //increment the token count
+  tokcount++;
+
+  //grab the next token
+  token = strtok(NULL, " ");
+ }
+ 
+ //put a 0 onto the end of the array and then return the parsed opts
+ strarray[tokcount] = 0;
  return strarray;
 }
--- cse109.154/Prog2/prog2.c
+++ cse109.154/Prog2/prog2.c
@@ -1,41 +1,66 @@
+/*
+ * CSE 109
+ * Calvin Tong
+ * cyt219
+ * implements a simple cli into which the user types simple unix commands which are parsed and executed by the program
+ * Program #2
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
+//prototype for the getopts methods which is defined in getopts.c
 char **getopts(char cmd[]);
-char buf[256];
-char hostname[256];
 
+/*
+ * the main method
+ * @param argc the number of command line args
+ * @#param argv the array containg the cmdline args
+ */
 int main(int argc, char *argv[])
 {
+ //the string which holds the command read from stdin
+ char buf[256];
+
+ //the string that holds the name of the host computer 
+ char hostname[100];
+ 
+ //gets the host name of the computer. Only runs once 
+ //because we are assmuing host machine will not change during runtime
  gethostname(hostname, 256);
  
+ //infinite loop, breaks once the user types exit
  while(1)
  {
+  //prompt the user with a string with format  hostname>
   printf("%s> ", hostname);
+  
+  //read in the command from stdin and store it in buf
   fgets(buf, 256, stdin);
+  
+  //chop off the \n character that is read in by fgets
   int len = strlen(buf);
   buf[len - 1] = '\0';
- 
-  printf("chopy chopy %s\n", buf);
-
+  
+  //if the user typed in exit then break out of the loop
   if(strcmp(buf, "exit") == 0)
   {
-   return 0;
+   break;
   }
   
-  printf("cmp cmp\n");
-
+  //run the getopts method to parse command line args and assign
+  //the parsed args to argv
   argv = getopts(buf);
   
-  printf("got le opts\n");
-  
-  if(fork() == 0)
+  //not my code, taken from the assignment page
+  //runs the parsed command line args
+  if (fork() == 0)
   {
    execvp(argv[0], argv);
-  }
-  else
+  } 
+  else 
   {
    wait(NULL);
   }
