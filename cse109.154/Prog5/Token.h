03107ed0a4af6ee9eb888bf0b7b31e83fa5d8170
ctong1234 <calvinytong@gmail.com>
2015-09-30 21:37:17 -0400

    asdf

--- cse109.154/Prog5/Token.h
+++ cse109.154/Prog5/Token.h
@@ -10,41 +10,41 @@ class Token
 {
  public:
   //header file declarations for the token types
-  static const int Token::INTLIT = 1;
-  static const int Token::FLOATLIT = 2;
-  static const int Token::STRLIT = 3;
-  static const int Token::IDENT = 4;
-  static const int Token::PLUS = 5;
-  static const int Token::MINUS = 6;
-  static const int Token::TIMES = 7;
-  static const int Token::DIVIDE = 8;
-  static const int Token::REM = 9;
-  static const int Token::ASSIGN = 10;
-  static const int Token::LPAREN = 11;
-  static const int Token::RPAREN = 12;
-  static const int Token::COMMA = 13;
-  static const int Token::EQ = 14;
-  static const int Token::LT = 15;
-  static const int Token::LE = 16;
-  static const int Token::GT = 17;
-  static const int Token::GE = 18;
-  static const int Token::NE = 19;
+  static const int INTLIT = 1;
+  static const int FLOATLIT = 2;
+  static const int STRLIT = 3;
+  static const int IDENT = 4;
+  static const int PLUS = 5;
+  static const int MINUS = 6;
+  static const int TIMES = 7;
+  static const int DIVIDE = 8;
+  static const int REM = 9;
+  static const int ASSIGN = 10;
+  static const int LPAREN = 11;
+  static const int RPAREN = 12;
+  static const int COMMA = 13;
+  static const int EQ = 14;
+  static const int LT = 15;
+  static const int LE = 16;
+  static const int GT = 17;
+  static const int GE = 18;
+  static const int NE = 19;
 
-  static const int Token::SET = 20;
-  static const int Token::PRINT = 21;
-  static const int Token::WHILE = 22;
-  static const int Token::DO = 23;
-  static const int Token::END = 24;
-  static const int Token::AND = 25;
-  static const int Token::OR = 26;
-  static const int Token::IF = 27;
-  static const int Token::THEN = 28;
-  static const int Token::ELSE = 29;
-  static const int Token::ENDIF = 30;
+  static const int SET = 20;
+  static const int PRINT = 21;
+  static const int WHILE = 22;
+  static const int DO = 23;
+  static const int END = 24;
+  static const int AND = 25;
+  static const int OR = 26;
+  static const int IF = 27;
+  static const int THEN = 28;
+  static const int ELSE = 29;
+  static const int ENDIF = 30;
   
-  static const int Token::PROGRAM = 31;
-  static const int Token::ENDOFFILE = 32;
-  static const int Token::ERROR = 33;
+  static const int PROGRAM = 31;
+  static const int ENDOFFILE = 32;
+  static const int ERROR = 33;
   //constructors
   Token(int t, string lex, int l, int p);
   Token();
