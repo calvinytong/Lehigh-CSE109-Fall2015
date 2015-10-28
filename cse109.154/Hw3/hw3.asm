3477ff4312e7af955e8f47d26e8843aa32123468
Calvin Tong <cyt219@ariel.cse.lehigh.edu>
2015-10-28 11:25:48 -0400

    final version of tokenizer

--- cse109.154/Hw3/hw3.asm
+++ cse109.154/Hw3/hw3.asm
@@ -1,3 +1,10 @@
+;CSE 109
+;Calvin Tong
+;cyt219
+;hw 3
+;tokenize a string
+
+
 SYS_EXIT equ 1
 SYS_WRITE equ 4
 STDOUT equ 1
@@ -13,14 +20,18 @@ itrstr:
   jz done                 ; jump to the end if we are at the end of the string
   cmp byte[ecx], 0x20     ; check if we have a space
   jz printlnlbl           ; jump to printlnlbl
-retprintln:  
   call writeout           ; write the char out, skipped if its a space
   inc ecx                 ; increment the pointer stored in ecx
   jmp itrstr              ; jump back to the top of the loop
 
 printlnlbl: 
   mov [ecx], byte 0xA     ; move the new line character to ecx as the character to print
-  jmp retprintln          ; jump back to the loop after the other call to write out
+  call writeout           ; print the space
+top:
+  inc ecx                 ; increment the pointer
+  cmp byte[ecx], 0x20     ; check if space is there
+  jz top                  ; if it is a space then inc pointer and check if space again 
+  jmp itrstr             ; jump back to the loop @the next not space char
 
 done:
   mov ecx, NL            
@@ -36,5 +47,5 @@ writeout:
   ret
 
 segment .data
-  MSG db "this is a string with tokens hi hi hi", 0x0 ; string to write out
+  MSG db "this   is   a  string with tokens hi hi hi", 0x0 ; string to write out
   NL db 0xA
