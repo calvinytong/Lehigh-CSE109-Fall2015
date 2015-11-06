494537f2ceb7ca9f0794ce90cbb67e72b920eef5
Calvin Tong <cyt219@pan.cse.lehigh.edu>
2015-11-06 11:26:53 -0500

    asdf

--- cse109.154/Prog7/assembler.cpp
+++ cse109.154/Prog7/assembler.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "Trie.h"
-
+#include <string.h>
+#include "Lexer.h"
 #define numops 22
+
+using namespace std;
 
 string ops[] = {"ADD", "SUB", "MULT", "DIV", "REM", "DUP", "AND", "OR", "NOT", "ISEQ", "ISNE", 
 "ISGT", "ISGE", "ISLT", "ISLE", "LOADL", "LOADV", "STOREV", "JUMP", "JUMPF", "PRINT", "PRINTLN"
@@ -11,6 +15,7 @@ string ops[] = {"ADD", "SUB", "MULT", "DIV", "REM", "DUP", "AND", "OR", "NOT", "
 string codes[] = {"A0", "A1", "A3", "A4", "A5", "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7",
 "B8", "C0", "C1", "C2", "D0", "D1", "E0", "E1", "E2"};
 
+string hexcode = "CAFEBE";
 
 Trie *makeInsTable()
 {
@@ -25,10 +30,47 @@ Trie *makeInsTable()
 
 int main(int argc, char** argv)
 {
- //char *program[255];
+ ifstream fin;
+ istream iin;
+ Lexer l;
+ //init tables
+ string program[255];
+ program[0] = hexcode;
  //Trie *labelmap = new Trie();
  //Trie *argmap = new Trie();
  Trie *instable = makeInsTable();
- cout << instable;
+ 
+ //parse cmdline options
+ for(int i = 1; i < argc; i++)
+ {
+  if(!strcmp(argv[i], "-h"))
+  {
+   continue;
+  }
+
+  if(!strcmp(argv[i], "-i"))
+  {
+   continue;
+  }
+
+  if(!strcmp(argv[i], "-l"))
+  {
+   continue;
+  }
+
+  if(!strcmp(argv[i], "-a"))
+  {
+   continue;
+  }
+
+  if(!strcmp(argv[i], "p"))
+  {
+   continue;
+  }
+
+  //pseudo else condition b/c of continues
+  l = new Lexer(iin);
+ }
+ 
  return 0;
 }
